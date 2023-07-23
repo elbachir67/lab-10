dff330ebe8ae88e47d195ce5dc0d83992bbf577e
bass <bassirou.toure@esp.sn>
2023-07-23 22:57:33 +0000

    position_final

---
+++ src/Meteo.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import "./Meteo.css";
+function Meteo({ longitude, latitude }) {
+  const [city, setCity] = useState("");
+  const [temp, setTemp] = useState("");
+  const [main, setMain] = useState("");
+  const [desc, setDesc] = useState("");
+  const [sunrise, setSunrise] = useState("");
+  const [sunset, setSunset] = useState("");
+
+  const [icon, setIcon] = useState("");
+  const [isReady, setReady] = useState(false);
+  React.useEffect(() => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${longitude}&lon=${latitude}&appid=485eaae13c44dca81805beb5cfe0e7b3&units=metric`
+    )
+      .then(result => result.json())
+      .then(jsonresult => {
+        setCity(jsonresult.name);
+        setTemp(jsonresult.main.temp);
+        setMain(jsonresult.weather[0].main);
+        setDesc(jsonresult.weather[0].description);
+        setIcon(jsonresult.weather[0].icon);
+        setSunrise(jsonresult.sys.sunrise);
+        setSunset(jsonresult.sys.sunset);
+        setReady(true);
+      })
+      .catch(err => console.error(err));
+  }, [longitude, latitude]);
+  if (isReady) {
+    const dateset = new Date(sunrise * 1000);
+    let hours =
+      dateset.getUTCHours() < 10
+        ? "0" + dateset.getUTCHours()
+        : dateset.getUTCHours();
+
+    let minutes =
+      dateset.getUTCMinutes() < 10
+        ? "0" + dateset.getUTCMinutes()
+        : dateset.getUTCMinutes();
+    let secondes =
+      dateset.getUTCSeconds() < 10
+        ? "0" + dateset.getUTCSeconds()
+        : dateset.getUTCSeconds();
+
+    const datetimeset = hours + ":" + minutes + ":" + secondes;
+
+    const daterise = new Date(sunset * 1000);
+    hours =
+      daterise.getUTCHours() < 10
+        ? "0" + daterise.getUTCHours()
+        : daterise.getUTCHours();
+    minutes =
+      daterise.getUTCMinutes() < 10
+        ? "0" + daterise.getUTCMinutes()
+        : daterise.getUTCMinutes();
+    secondes =
+      daterise.getUTCSeconds() < 10
+        ? "0" + daterise.getUTCSeconds()
+        : daterise.getUTCSeconds();
+    const datetimerise = hours + ":" + minutes + ":" + secondes;
+
+    return (
+      <dl className="App alert alert-success">
+        <dt>City</dt>
+        <dd>{city} </dd>
+        <dt>Temperature</dt>
+        <dd>{temp} °C</dd>
+        <dt>Main</dt>
+        <dd>{main}</dd>
+        <dt>Description</dt>
+        <dd>{desc}</dd>
+        <dt>Illustration</dt>
+        <dd>
+          <img
+            src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+            alt="Weather icon"
+          />
+        </dd>
+        <dt>Sunrise </dt>
+        <dd>{datetimeset} </dd>
+        <dt>Sunset </dt>
+        <dd>{datetimerise} </dd>
+      </dl>
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
+}
+
+export default Meteo;
+
+// london : 51.5085, -0.1257
---
+++ src/MyForm.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./MyForm.css";
+import Meteo from "./Meteo";
+function MyForm() {
+  const [position, setPosition] = useState({
+    longitude: 14.1825,
+    latitude: -16.2533,
+  });
+
+  // Enregistrer la valeur de la zone de saisie dans l'état lorsqu'elle est modifiée
+  const champModifie = event => {
+    setPosition({ ...position, [event.target.name]: event.target.value });
+  };
+  const handleSubmit = event => {
+    alert(`Position ${position.longitude} ${position.latitude}`);
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <form>
+        <label>Longitude :</label>
+        <input
+          type="text"
+          name="longitude"
+          onChange={champModifie}
+          value={position.longitude}
+        />
+        <br />
+        <label>Latitude :</label>
+        <input
+          type="text"
+          name="latitude"
+          onChange={champModifie}
+          value={position.latitude}
+        />
+      </form>
+
+      <Meteo longitude={position.longitude} latitude={position.latitude} />
+    </>
+  );
+}
+export default MyForm;
